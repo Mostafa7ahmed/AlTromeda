d2099023d4936088d6ead9d99c6f8509ca746dd6
mostafa hamed <Mostafa7ahmed>
2025-08-25 17:20:44 +0300

    feat: Update navbar links and add new pages (about, services, projects, teams, contact) with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,23 +3,44 @@ import { authGuard } from './Core/guards/auth.guard';
 import { isAuthGuard } from './Core/guards/is-auth.guard';
 
 export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
 
-    {
-        path: 'home',
-        loadComponent: () => import('./Pages/home/home').then(m => m.Home),
-    },
-    {
-        path: 'login',
-        loadComponent: () => import('./Pages/Auth/login/login').then(m => m.Login),
-        canActivate: [isAuthGuard]
-    },
-    {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'home'
-    },
-    {
-        path: '**',
-        redirectTo: 'home'
-    }
+  {
+    path: 'home',
+    loadComponent: () => import('./Pages/home/home').then((m) => m.Home),
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./Pages/about/about').then((m) => m.About),
+  },
+  {
+    path: 'services',
+    loadComponent: () => import('./Pages/services/services').then((m) => m.Services),
+  },
+  {
+    path: 'projects',
+    loadComponent: () => import('./Pages/projects/projects').then((m) => m.Projects),
+  },
+  {
+    path: 'teams',
+    loadComponent: () => import('./Pages/teams/teams').then((m) => m.Teams),
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./Pages/contact/contact').then((m) => m.Contact),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./Pages/Auth/login/login').then((m) => m.Login),
+    canActivate: [isAuthGuard],
+  },
+
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
 ];
