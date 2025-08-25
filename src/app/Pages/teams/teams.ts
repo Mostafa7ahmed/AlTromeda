667d39f1aa3e9a77a64b84bfcec41b24a1c6547a
mostafa hamed <Mostafa7ahmed>
2025-08-25 20:55:42 +0300

    Teams

--- src/app/Pages/teams/teams.ts
+++ src/app/Pages/teams/teams.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PageHeader } from "../../components/page-header/page-header";
 
+import * as AOS from 'aos';
 @Component({
   selector: 'app-teams',
   imports: [PageHeader],
@@ -8,5 +9,7 @@ import { PageHeader } from "../../components/page-header/page-header";
   styleUrl: './teams.scss'
 })
 export class Teams {
-
+ ngOnInit(): void {
+    AOS.init({ duration: 1000, once: true });
+  }
 }
