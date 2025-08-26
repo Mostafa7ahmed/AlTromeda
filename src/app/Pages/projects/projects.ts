4461c15636c9d130cff14f555891467bf51ec850
mostafa hamed <Mostafa7ahmed>
2025-08-26 19:39:07 +0300

    feat(projects): implement project filtering and add post details with animations

--- src/app/Pages/projects/projects.ts
+++ src/app/Pages/projects/projects.ts
@@ -1,12 +1,71 @@
 import { Component } from '@angular/core';
 import { PageHeader } from "../../components/page-header/page-header";
+import { CommonModule } from '@angular/common';
 
+import * as AOS from 'aos';
 @Component({
   selector: 'app-projects',
-  imports: [PageHeader],
+  imports: [PageHeader , CommonModule],
   templateUrl: './projects.html',
   styleUrl: './projects.scss'
 })
 export class Projects {
+ categories: string[] = [
+    'All', 'Plumbing', 'Architecture', 'Design', 'Maintenance'
+  ];
 
+  selectedCategory: string = 'All';
+
+  posts = [
+    {
+      image: 'Image/uiux.jpg',
+      date: '19 Jan 2023',
+      title: 'Discovering Eco-Friendly Solutions for a Your Home',
+      excerpt: 'Discover the ultimate guide to troubleshooting smart home issues...',
+      tags: ['Plumbing', 'Architecture', 'Maintenance']
+    },
+    {
+      image: 'Image/uiux.jpg',
+      date: '22 Jan 2023',
+      title: '10 Essential Plumbing Tips for Every Homeowner',
+      excerpt: 'Ten valuable plumbing tips that every homeowner should know...',
+      tags: ['Plumbing', 'Maintenance']
+    },
+    {
+      image: 'Image/uiux.jpg',
+      date: '10 Feb 2023',
+      title: 'A Step-by-Step Guide to Electrical Safety at Home',
+      excerpt: 'Ensure the safety of your family with this comprehensive guide...',
+      tags: ['Electrical', 'System', 'Security']
+    },
+    {
+      image: 'Image/uiux.jpg',
+      date: '14 Feb 2023',
+      title: 'Smart Gardening Tips for Beginners',
+      excerpt: 'Transform your backyard into a green paradise with these tips...',
+      tags: ['Gardening', 'Design']
+    },
+    {
+      image: 'Image/uiux.jpg',
+      date: '25 Feb 2023',
+      title: 'Modern Painting Techniques for Your Living Room',
+      excerpt: 'Bring new life to your home interior with these techniques...',
+      tags: ['Painting', 'Design']
+    }
+  ];
+
+  filteredPosts = [...this.posts];
+
+  ngOnInit() {
+    AOS.init();
+  }
+
+  filterPosts(category: string) {
+    this.selectedCategory = category;
+    if (category === 'All') {
+      this.filteredPosts = [...this.posts];
+    } else {
+      this.filteredPosts = this.posts.filter(post => post.tags.includes(category));
+    }
+  }
 }
--- src/app/Pages/services/services.ts
+++ src/app/Pages/services/services.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { PageHeader } from "../../components/page-header/page-header";
+import { Future } from "../../components/future/future";
 
 @Component({
   selector: 'app-services',
-  imports: [PageHeader],
+  imports: [PageHeader, Future],
   templateUrl: './services.html',
   styleUrl: './services.scss'
 })
