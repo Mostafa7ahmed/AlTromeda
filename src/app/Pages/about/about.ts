3e133799e971084f894062410b3f5b72002c5b68
mostafa hamed <Mostafa7ahmed>
2025-08-26 16:23:26 +0300

    About

--- src/app/Pages/about/about.ts
+++ src/app/Pages/about/about.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { PageHeader } from "../../components/page-header/page-header";
-
+import * as AOS from 'aos';
 @Component({
   selector: 'app-about',
   imports: [PageHeader],
@@ -8,5 +8,7 @@ import { PageHeader } from "../../components/page-header/page-header";
   styleUrl: './about.scss'
 })
 export class About {
-
+  ngOnInit(): void {
+    AOS.init({ duration: 1000, once: true });
+  }
 }
