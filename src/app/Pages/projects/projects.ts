9d3569fb82975f96262b1a6fe93591ab4663e23e
mostafa hamed <Mostafa7ahmed>
2025-08-27 20:33:27 +0300

    Done

--- src/app/Pages/projects/projects.ts
+++ src/app/Pages/projects/projects.ts
@@ -10,9 +10,13 @@ import * as AOS from 'aos';
   styleUrl: './projects.scss'
 })
 export class Projects {
- categories: string[] = [
-    'All', 'Plumbing', 'Architecture', 'Design', 'Maintenance'
-  ];
+categories: string[] = [
+  'All', 
+  'Web Application', 
+  'Mobile Application', 
+  'Web & Mobile', 
+  'UI/UX Design',
+];
 
   selectedCategory: string = 'All';
 
@@ -22,35 +26,35 @@ export class Projects {
       date: '19 Jan 2023',
       title: 'Discovering Eco-Friendly Solutions for a Your Home',
       excerpt: 'Discover the ultimate guide to troubleshooting smart home issues...',
-      tags: ['Plumbing', 'Architecture', 'Maintenance']
+    tags: ['Web Application']
     },
     {
       image: 'Image/uiux.jpg',
       date: '22 Jan 2023',
       title: '10 Essential Plumbing Tips for Every Homeowner',
       excerpt: 'Ten valuable plumbing tips that every homeowner should know...',
-      tags: ['Plumbing', 'Maintenance', 'Plumbing', 'Maintenance', 'Plumbing', 'Maintenance']
+    tags: ['Mobile Application', 'Design', 'Maintenance']
     },
     {
       image: 'Image/uiux.jpg',
       date: '10 Feb 2023',
       title: 'A Step-by-Step Guide to Electrical Safety at Home',
       excerpt: 'Ensure the safety of your family with this comprehensive guide...',
-      tags: ['Electrical', 'System', 'Security']
+    tags: ['Web & Mobile', 'System', 'Security']
     },
     {
       image: 'Image/uiux.jpg',
       date: '14 Feb 2023',
       title: 'Smart Gardening Tips for Beginners',
       excerpt: 'Transform your backyard into a green paradise with these tips...',
-      tags: ['Gardening', 'Design']
+    tags: ['UI/UX Design',]
     },
     {
       image: 'Image/uiux.jpg',
       date: '25 Feb 2023',
       title: 'Modern Painting Techniques for Your Living Room',
       excerpt: 'Bring new life to your home interior with these techniques...',
-      tags: ['Painting', 'Design']
+    tags: ['UI/UX Design',]
     }
   ];
 
